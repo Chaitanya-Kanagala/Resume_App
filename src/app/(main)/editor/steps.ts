bfa446e68845d98a1c5d911e1a463c398ad4ed1e
Chaitanya Kanagala <43912509+Chaitanya-Kanagala@users.noreply.github.com>
2025-03-19 02:31:52 -0400

    Code updated till work exp section in forms
    Implemented Code till work experience section

---
+++ src/app/(main)/editor/steps.ts
@@ -0,0 +1,15 @@
+import React from "react";
+import GeneralInfoForm from "./forms/GeneralInfoForm";
+import PersonalInfoForm from "./forms/PersonalInfoForm";
+import { EditorFormProps } from "@/lib/types";
+import WorkExperienceForm from "./forms/WorkExperienceForm";
+
+export const steps: {
+    title: string;
+    component: React.ComponentType<EditorFormProps>;
+    key: string;
+}[] = [
+    {title: "General info", component: GeneralInfoForm, key: "general-info" },
+    {title: "Personal info", component: PersonalInfoForm, key: "personal-info"},
+    {title: "Work experience", component:WorkExperienceForm, key: "work-experience"}
+];
