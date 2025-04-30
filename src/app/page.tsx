e32a0f187e4430982caff0b47a89625363f08fbf
Chaitanya Kanagala <43912509+Chaitanya-Kanagala@users.noreply.github.com>
2025-04-30 18:37:05 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import logo from "@/assets/logo.png";
-import resumePreview from "@/assets/resume-preview.jpg";
+import resumePreview from "@/assets/resume-preview.jpeg";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
